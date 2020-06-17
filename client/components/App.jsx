b76f06043f7f83f0ae2cc584f5b0ab6657c06822
Eric Su <esericsu@gmail.com>
2020-06-16 21:30:19 -0700

    server connect

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 
 const App = () => {
   return (
-    <div>React Works</div>
+    <div>
+      <h2>User Reviews</h2>
+      React Works
+    </div>
   );
 };
 
--- server/index.js
+++ server/index.js
@@ -1,2 +1,18 @@
+const express = require('express');
 
-console.log('node works')
+const app = express();
+const path = require('path');
+const bodyParser = require('body-parser');
+const port = process.env.PORT || 3009;
+const url= `http://localhost:${3009}`;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '../public')));
+
+
+
+app.listen(port, () => {
+  console.log(`Listening at ${url}`);
+})
+console.log('node works in root');
